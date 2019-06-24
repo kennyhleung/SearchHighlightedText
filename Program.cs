cbce1965b6a38877e26794075646c6c0d1b2f097
kennyhleung <kennyhleung@gmail.com>
2019-06-24 11:24:43 -0500

    Additional shortcuts and use Ctrl
    alpha (alt+A), beta (alt+B), gamma (alt+G), up arrow (ctrl+'), down arrow (ctrl+;), right arrow (clrl+L)
    updating to use ctrl instead of alt for searches, as having alt still pressed down does not enable copying and pasting
    ctrl+[ for searching highlighted text as normal google search
    ctrl+] for searching highlighted text as image search

--- Program.cs
+++ Program.cs
@@ -11,7 +11,21 @@ namespace SearchHighlightedText
     static class Program
     {
 
-     private static int hotKeyNumber;
+        private static int hotKeyNumber;
+        private static int hotKeyNumber2;
+        private static int beta;
+        private static int gamma;
+        private static int alpha;
+        private static int delta;
+
+        private static int hotKeyNumberImage;
+        private static int hotKeyNumberImage2;
+        private static int insertIncreaseArrow;
+        private static int insertIncreaseArrow2;
+        private static int insertDecreaseArrow;
+        private static int insertDecreaseArrow2;
+        private static int insertForwardArrow;
+        private static int insertForwardArrow2;
 
         [STAThread]
         static void Main()
@@ -33,24 +47,116 @@ namespace SearchHighlightedText
             systemTray.Visible = true;
 
             hotKeyNumber = HotKeyManager.RegisterHotKey(Keys.C, KeyModifiers.Alt);
+            hotKeyNumberImage = HotKeyManager.RegisterHotKey(Keys.X, KeyModifiers.Alt);
+            alpha = HotKeyManager.RegisterHotKey(Keys.A, KeyModifiers.Alt);
+            beta = HotKeyManager.RegisterHotKey(Keys.B, KeyModifiers.Alt);
+            delta = HotKeyManager.RegisterHotKey(Keys.D, KeyModifiers.Alt);
+            gamma = HotKeyManager.RegisterHotKey(Keys.G, KeyModifiers.Alt);
+            insertIncreaseArrow = HotKeyManager.RegisterHotKey(Keys.Up, KeyModifiers.Alt);
+            insertDecreaseArrow = HotKeyManager.RegisterHotKey(Keys.Down, KeyModifiers.Alt);
+            insertForwardArrow = HotKeyManager.RegisterHotKey(Keys.Right, KeyModifiers.Alt);
+
+            hotKeyNumber2 = HotKeyManager.RegisterHotKey(Keys.Oem4, KeyModifiers.Control);
+            hotKeyNumberImage2 = HotKeyManager.RegisterHotKey(Keys.Oem6, KeyModifiers.Control);
+            insertIncreaseArrow2 = HotKeyManager.RegisterHotKey(Keys.OemQuotes, KeyModifiers.Control);
+            insertDecreaseArrow2 = HotKeyManager.RegisterHotKey(Keys.OemSemicolon, KeyModifiers.Control);
+            insertForwardArrow2 = HotKeyManager.RegisterHotKey(Keys.L, KeyModifiers.Control);
+
             HotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(CopyKeyboardSearch);
+
             Application.Run();
         }
 
         static void CopyKeyboardSearch(object sender, HotKeyEventArgs e)
         {
-            SendKeys.Send("^c");
-            Thread.Sleep(100);
-            SendKeys.Send("^c");
-            Thread.Sleep(100);
-            string lastClipboardText = Clipboard.GetText();
-            if (lastClipboardText != "")
-                Process.Start("https://www.google.com/search?q=" + lastClipboardText.TrimStart(' ').TrimEnd(' ').Replace(" ", "+"));
+            switch (e.Key)
+            {
+
+                case Keys.Down: 
+                case Keys.OemSemicolon:
+                    Thread.Sleep(200);
+                    SendKeys.Send('\u2193'.ToString());
+                    //SendKeys.Send(' '.ToString());
+                    break;
+
+                case Keys.Up:
+                case Keys.OemQuotes:
+                    Thread.Sleep(200);
+                    SendKeys.Send('\u2191'.ToString());
+                    //SendKeys.Send(' '.ToString());
+                    break;
+
+                case Keys.L:
+                case Keys.Right:
+                    Thread.Sleep(200);
+                    SendKeys.Send('\u2192'.ToString());
+                    SendKeys.Send(' '.ToString());
+                    break;
+
+                case Keys.Oem4:
+                case Keys.C:
+                    //Thread.Sleep(500);
+                    SendKeys.Send("^c");
+                    Thread.Sleep(200);//SendKeys.Send("^c");                    
+                    string lastClipboardText = Clipboard.GetText();
+                    if (lastClipboardText != "")
+                        Process.Start("https://www.google.com/search?q=" +
+                                      lastClipboardText.TrimStart(' ').TrimEnd(' ').Replace(" ", "+"));
+                    break;
+                case Keys.Oem6:
+                case Keys.X:
+                    //Thread.Sleep(500);
+                    //SendKeys.Send("^c");
+                    SendKeys.Send("^c");
+                    Thread.Sleep(200);
+                    string imageText = Clipboard.GetText();
+                    if (imageText != "")
+                        Process.Start("https://www.google.com/search?tbm=isch&q=" +
+                                      imageText.TrimStart(' ').TrimEnd(' ').Replace(" ", "+"));
+                    break;
+
+                case Keys.A:
+                    Thread.Sleep(200);
+                    SendKeys.Send('\u03b1'.ToString());
+                    break;
+
+                case Keys.B:
+                    Thread.Sleep(200);
+                    SendKeys.Send('\u03b2'.ToString());
+                    break;
+
+                case Keys.D:
+                    Thread.Sleep(200);
+                    SendKeys.Send('\u03b4'.ToString());
+                    break;
+
+                case Keys.G:
+                    Thread.Sleep(200);
+                    SendKeys.Send('\u03b3'.ToString());
+                    break;
+
+                default:
+                    break;
+            }
+
         }
 
         static void Exit_Click(object sender, EventArgs e)
         {
             HotKeyManager.UnregisterHotKey(hotKeyNumber);
+            HotKeyManager.UnregisterHotKey(hotKeyNumberImage);
+            HotKeyManager.UnregisterHotKey(alpha);
+            HotKeyManager.UnregisterHotKey(beta);
+            HotKeyManager.UnregisterHotKey(delta);
+            HotKeyManager.UnregisterHotKey(gamma);
+            HotKeyManager.UnregisterHotKey(insertIncreaseArrow);
+            HotKeyManager.UnregisterHotKey(insertDecreaseArrow);
+            HotKeyManager.UnregisterHotKey(insertForwardArrow);
+            HotKeyManager.UnregisterHotKey(hotKeyNumber2);
+            HotKeyManager.UnregisterHotKey(hotKeyNumberImage2);
+            HotKeyManager.UnregisterHotKey(insertIncreaseArrow2);
+            HotKeyManager.UnregisterHotKey(insertDecreaseArrow2);
+            HotKeyManager.UnregisterHotKey(insertForwardArrow2);
             Application.Exit();
         }
 
